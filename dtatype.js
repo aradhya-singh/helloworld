46b769c19cad5cec0ca8337ce394154c7d4b0856
aradhya-singh <aradhya.7484@gmail.com>
2024-06-07 13:41:23 +0530

    done

--- dtatype.js
+++ dtatype.js
@@ -1,7 +1,44 @@
-let age=22
-let password
-console.log(typeof "Aradhya");
-console.log(typeof age)
-console.log(typeof password)
-console.log(typeof null)
-console.log(typeof 2345)
+// let age=22
+// let password
+// console.log(typeof "Aradhya");
+// console.log(typeof age)
+// console.log(typeof password)
+// console.log(typeof null)
+// console.log(typeof 2345)
+
+// let value =3
+// let negvalue=-value
+// console.log(negvalue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+// console.log(2>1)
+// console.log(2>=2)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=2)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+// console.log(null > 0)
+// console.log(null==0)
+// console.log(null>=0);
+
+// strict check
+//console.log("2" === 2)
+ let userone={
+    email: "aradhya@google.com"
+    upi : "user@b1"
+ }
+ console.log(userone)
+ let usertwo=userone
+ usertwo.email: "aradhyasingh@google.com"
+ console.log(usertwo)
+ console.log(userone)
---
+++ stackheap.js/stackheap.js
@@ -0,0 +1,10 @@
+let userone={
+    email: "aradhya@google.com",
+    upi: "user@b1"
+ }
+ console.log(userone)
+ let usertwo=userone
+ usertwo.email= "aradhyasingh@google.com"
+ console.log(usertwo)
+ console.log(userone)
+ 
